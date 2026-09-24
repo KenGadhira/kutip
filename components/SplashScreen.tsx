ae93dfd69e1d417605f9c736aa096babc110389e
FunnyClowns <faradeaken@gmail.com>
2026-09-24 13:17:09 +0700

    refactor(splash): use kutip-splash theme color

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -35,7 +35,7 @@ export default function SplashScreen({
 
   const content = (
     <div
-      className="w-full sm:max-w-[375px] h-screen sm:h-[812px] bg-[#758c3b] sm:rounded-[36px] sm:shadow-2xl overflow-hidden flex flex-col items-center justify-center relative mx-auto select-none border border-black/5"
+      className="w-full sm:max-w-[375px] h-screen sm:h-[812px] bg-kutip-splash sm:rounded-[36px] sm:shadow-2xl overflow-hidden flex flex-col items-center justify-center relative mx-auto select-none border border-black/5"
       data-node-id="98:3"
       data-name="Splash Screen"
     >
